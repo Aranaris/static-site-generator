d6d02cc41d57aea4ca7ef4476116892fb279134f
aranaris <vinceye.cm@gmail.com>
2025-02-23 13:47:00 -0800

    initial project setup and create TextNode obj

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode
+
+def main():
+	new_node = TextNode("This is a text node", "bold", url="https://www.boot.dev")
+	print(new_node)
+
+if __name__ == "__main__":
+	main()
---
+++ src/textnode.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+class TextType(Enum):
+	NORMAL_TEXT = "normal"
+	BOLD_TEXT = "bold"
+	ITALIC_TEXT = "italic"
+	CODE_TEXT = "code"
+	LINKS = "links"
+	IMAGES = "images"
+
+
+class TextNode():
+	def __init__(self, text:str, text_type:str, **kwargs) -> None:
+		self.text = text
+		self.text_type = TextType(text_type)
+		self.url = kwargs.get("url", None)
+
+	def __eq__(self, value: object) -> bool:
+		if self.text == value.text and self.text_type == value.text_type and self.url == value.url:
+			return True
+		return False
+	
+	def __repr__(self) -> str:
+		return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
