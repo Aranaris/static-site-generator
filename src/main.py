c790932b1b3fe99fdf67ad4b50a9dbaa559005f6
aranaris <vinceye.cm@gmail.com>
2025-04-26 14:36:18 -0700

    add copy to static dir func in main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,39 @@
 from textnode import TextNode
+import os, shutil
 
 def main():
-	new_node = TextNode("This is a text node", "bold", url="https://www.boot.dev")
-	print(new_node)
+	with open("run.log", "w") as log_file:
+		copy_to_dir("static","public",log_file)
 
+def copy_to_dir(src_dir:str, dest_dir:str, log_file:object) -> None:
+	if not os.path.exists(dest_dir):
+		raise FileNotFoundError(f"directory {dest_dir} does not exist")
+	
+	for filename in os.listdir(dest_dir):
+		pathname = os.path.join(dest_dir, filename)
+		if os.path.isfile(pathname):
+			log_file.write(f"Deleting file {pathname}...\n")
+			os.unlink(pathname)
+		elif os.path.isdir(pathname):
+			shutil.rmtree(pathname)
+			log_file.write(f"Deleting directory {pathname}...\n")
+		else:
+			raise RuntimeError(f"unable to remove file {pathname}")
+	
+	src_files = os.listdir(src_dir)
+	for filename in src_files:
+		pathname = os.path.join(src_dir, filename)
+		if os.path.isfile(pathname):
+			log_file.write(f"Copying file {pathname} to {dest_dir}...\n")
+			shutil.copy(pathname, dest_dir)
+			
+		elif os.path.isdir(pathname):
+			dest_pathname = os.path.join(dest_dir, filename)
+			log_file.write(f"Creating directory {dest_pathname}...\n")
+			os.mkdir(dest_pathname)
+			copy_to_dir(pathname, dest_pathname, log_file)
+		else:
+			raise RuntimeError(f"unable to copy file {pathname}")
+	
 if __name__ == "__main__":
 	main()
